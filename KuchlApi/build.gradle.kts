89888fc91fb3587eca2d5ff027b69493106bf4ee
Simon Deutsch <simon.deutsch@upstream-mobility.at>
2025-05-20 14:59:55 +0200

    remove linux and jvm from gradle

--- KuchlApi/build.gradle.kts
+++ KuchlApi/build.gradle.kts
@@ -13,7 +13,6 @@ group = "io.github.kotlin"
 version = "1.0.0"
 
 kotlin {
-    jvm()
     androidTarget {
         publishLibraryVariants("release")
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
@@ -24,7 +23,6 @@ kotlin {
     iosX64()
     iosArm64()
     iosSimulatorArm64()
-    linuxX64()
 
     sourceSets {
         val commonMain by getting {
